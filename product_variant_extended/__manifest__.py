011d3fd35cf703c54d56075ccf13ec8bb511d2a0
Graeme Gellatly <gdgellatly@gmail.com>
2016-10-13 10:17:31 +1300

    [INITIAL] Test addon for product variant enhancements

---
+++ product_variant_extended/__manifest__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Test Product Variant Enhancements',
+    'sequence': 120,
+    'version': '1.0',
+    'depends': ['base', 'product'],
+    'category': 'Sales',
+    'summary': 'Product Variant Enhancements',
+    'description': """
+This is a test module product variants.
+======================================
+
+Objectives
+Search in variant
+Order variants correctly
+Custom partcodes
+    """,
+    'data': [],
+    'qweb': [],
+    'installable': True,
+}
