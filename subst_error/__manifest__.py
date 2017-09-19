28eddbc8d874b4576229a2b7f2a28d86d9995842
Graeme Gellatly <gdgellatly@gmail.com>
2017-09-19 15:46:28 +1200

    Test for issue

---
+++ subst_error/__manifest__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 Graeme Gellatly
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'Fonts Qweb PDF Test',
+    'description': """
+        Fonts for RIL""",
+    'version': '11.0.1.0.0',
+    'license': 'AGPL-3',
+    'author': 'Graeme Gellatly',
+    'website': 'https://o4sb.com',
+    'depends': ['web'
+    ],
+    'data': ['views/assets.xml'
+    ],
+    'demo': [
+    ],
+}
